608886999e78623cbfa302ee67d936db2e58e770
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-07-01 23:07:15 +0530

    modify the slash command to let people ping me through the slash command

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 require("dotenv").config();
 
-const { App } = require("@slack/bolt");
+const{App} = require("@slack/bolt");
+const {WebClient} = require("@slack/web-api");
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
 });
 
-app.command("/ping-dinos-ping", async ({ command, ack, respond }) => {
-  const start = Date.now();
-  await ack();
-  const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+const client = new WebClient(process.env.SLACK_BOT_TOKEN)
+
+app.command("/ping-dinos", async({command, ack}) => {
+    await ack();
+
+    const arg = command.text.trim().toLowerCase();
+    const text = "<@U0AK24A5EA2> get pung!!!";
+
+    await client.chat.postMessage({
+        channel: command.channel_id,
+        text,
+        link_names: true,
+        as_user: true,
+    });
 });
 
 (async () => {
-  await app.start();
-  console.log("bot is running!");
+    await app.start();
+    console.log("bot is running!");
 })();
